2945515250bff12b175960f0dbb4da28d2d702ae
Nicolsky <35692364+NicoNiconiicolas@users.noreply.github.com>
2018-06-01 15:54:56 +0200

    Update index.php

--- index.php
+++ index.php
@@ -5,13 +5,12 @@
 	require "controller/mainController.php";
 	require "vues/footer.php";
 ?>
-
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400">
-    <link rel="stylesheet" href="font-awesome-4.5.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/magnific-popup.css">
-    <link rel="stylesheet" href="css/templatemo-style.css">
-
-    <script src="https://www.atlasestateagents.co.uk/javascript/tether.min.js"></script>
+	<link rel="stylesheet" href="font-awesome-4.5.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/magnific-popup.css">
+	<link rel="stylesheet" href="css/templatemo-style.css">
+	
+	<script src="https://www.atlasestateagents.co.uk/javascript/tether.min.js"></script>
 	<script src="js/bootstrap.min.js"></script>
 	<script src="js/jquery.magnific-popup.min.js"></script>
