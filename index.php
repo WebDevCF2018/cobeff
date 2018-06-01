a7297279781323c4dfb2202b73ab06fcc46490a6
Nicolsky <35692364+NicoNiconiicolas@users.noreply.github.com>
2018-06-01 15:33:03 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,17 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: nicolas.diarra
- * Date: 31/05/2018
- * Time: 11:51
- */
+	//Charger base fichier de db
+	
+	require "vues/header.php";
+	require "controller/mainController.php";
+	require "vues/footer.php";
+?>
+
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400">
+    <link rel="stylesheet" href="font-awesome-4.5.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+    <link rel="stylesheet" href="css/templatemo-style.css">
+
+    <script src="https://www.atlasestateagents.co.uk/javascript/tether.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/jquery.magnific-popup.min.js"></script>
