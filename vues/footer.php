fc85b2971b27bb4f9e6829483fbc995f416b4059
Nicolsky <35692364+NicoNiconiicolas@users.noreply.github.com>
2018-06-01 15:43:02 +0200

    Add files via upload

---
+++ vues/footer.php
@@ -0,0 +1,33 @@
+<div class="row tm-nav-2-row">
+    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+        <nav class="tm-flex-center">
+            <ul class="tm-nav-2">
+                <li class="tm-nav-item-2"><a href="./" class="tm-nav-link-2">Accueil</a></li>
+                <li class="tm-nav-item-2"><a href="./?p=formations" class="tm-nav-link-2">Formations</a></li>
+                <li class="tm-nav-item-2"><a href="./?p=about" class="tm-nav-link-2">Présentations</a></li>
+                <li class="tm-nav-item-2"><a href="./?p=actualités" class="tm-nav-link-2">Actualités</a></li>
+                <li class="tm-nav-item-2"><a href="./?p=contact" class="tm-nav-link-2">Contact</a></li>
+                <li class="tm-nav-item-2"><a href="./?p=restaurant" class="tm-nav-link-2">Restaurant</a></li>
+            </ul>
+        </nav>
+    </div>
+</div> <!-- row -->
+
+<div class="row">
+    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+        <div class="tm-social-icons-container text-xs-center">
+            <a href="#" class="tm-social-link"><i class="fa fa-facebook"></i></a>
+            <a href="#" class="tm-social-link"><i class="fa fa-linkedin"></i></a>
+        </div>
+    </div>
+</div>
+
+<footer class="row tm-footer">
+    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+        <p class="text-xs-center tm-copyright">Copyright &copy; 2016 Your Company
+
+        | Design: <a href="http://www.google.com/+templatemo" target="_parent">Templatemo</a></p>
+    </div>
+</footer>
+</body>
+</html>
---
+++ vues/header.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <title>Cobeff</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+<div class="row">
+    <div class="col-lg-12">
+        <nav class="navbar">
+                        
+            <div id="tmNavbar">
+                <div class="nav navbar-nav tm-nav">
+                                
+                    <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 col-xl-4 tm-pad-0 tm-nav-item-pair">
+                        <div id="nav-accueil" class="nav-item">
+                            <a class="nav-link" href="./">Accueil</a>
+                        </div>
+                        <div id="nav-about" class="nav-item">
+                            <a class="nav-link" href="./?p=about">Présentations</a>
+                        </div>    
+                    </div>
+                                
+                    <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 col-xl-4 tm-pad-0 tm-nav-item-pair">
+                        <div id="nav-formations" class="col-xs-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 nav-item">
+                            <a class="nav-link" href="./?p=formations">Formations</a>
+                        </div>
+                        <div id="nav-actualités" class="col-xs-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 nav-item">
+                            <a class="nav-link" href="./?p=actualités">Actualités</a>
+                        </div>    
+                    </div>
+
+                    <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 col-xl-4 tm-pad-0 tm-nav-item-pair">
+                        <div id="nav-contact" class="col-xs-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 nav-item">
+                            <a class="nav-link" href="./?p=contact">Contact</a>
+                        </div>
+                        <div id="nav-contact" class="col-xs-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 nav-item">
+                                <a class="nav-link" href="./?p=restaurant">Restaurant</a>
+                        </div>
+                        </div>
+                    </div>                            
+                                
+                </div>    
+            </div>                    
+        </nav>     
+    </div>       
+</div>
