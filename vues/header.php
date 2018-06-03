2f3b74afa1a7ac1c8b57ce09af66020b99b42237
Nicolsky <35692364+NicoNiconiicolas@users.noreply.github.com>
2018-06-03 12:56:53 +0200

    Affiche page lorsque seulement quand chargée

--- vues/header.php
+++ vues/header.php
@@ -5,9 +5,14 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <style>
+        body{
+            display: none;
+        }
+    </style>
 </head>
 
-<body>
+<body onload="document.body.style.display = 'block' ">
 <div class="row">
     <div class="col-lg-12">
         <nav class="navbar">
