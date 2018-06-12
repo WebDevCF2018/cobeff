f0c1257fba07ec32772a617064e6d69148fe8cac
Nicolsky <35692364+NicoNiconiicolas@users.noreply.github.com>
2018-06-12 14:17:13 +0200

    Add files via upload

---
+++ vues/try_connection.php
@@ -0,0 +1,29 @@
+<?php
+	session_start();
+	if(isset($_SESSION['username'])){
+		echo "Bienvenue ".$_SESSION['username']." !";
+		echo "<script>setTimeout(function(){location.replace('../');}, 3000)</script>";
+		}else if(isset($_GET['code'])){
+			switch($_GET['code']){
+				case 403:
+					echo "Identifiants incorrects";
+					echo "<script>setTimeout(function(){location.replace('../?p=login');}, 3000)</script>";
+					break;
+				case 2:
+					echo "Champ(s) incomplet(s)";
+					echo "<script>setTimeout(function(){location.replace('../?p=login');}, 3000)</script>";
+					break;
+
+				case 'what':
+					echo "Une erreur inconnue est survenue";
+					echo "<script>setTimeout(function(){location.replace('../?p=login');}, 3000)</script>";
+					break;
+
+				default:
+					header('Location: ../');
+			}
+
+		}else{
+			header('Location: ../');
+	}
+?>
