dabc6d186731c55d0a97c788b81482941dbe31b2
Nicolsky <35692364+NicoNiconiicolas@users.noreply.github.com>
2018-06-12 14:21:37 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -10,13 +10,11 @@
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400">
 	<link rel="stylesheet" href="font-awesome-4.5.0/css/font-awesome.min.css">
 	<link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/creative.min.css" >
-    <link rel="stylesheet" href="css/style.css">
 	<link rel="stylesheet" href="css/magnific-popup.css">
 	<link rel="stylesheet" href="css/templatemo-style.css">
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
     <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
 	<script src="https://www.atlasestateagents.co.uk/javascript/tether.min.js"></script>
-	<script src="js/bootstrap.min.js"></script>
+	<script src="js/ajax.js"></script>
+	<script src="js/jquery-1.11.3.min.js"></script>
 	<script src="js/jquery.magnific-popup.min.js"></script>
-
